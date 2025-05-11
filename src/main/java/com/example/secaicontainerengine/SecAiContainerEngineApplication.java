dd927e75cfb02d55130eab93b538844f273251a2
ChengFangZheng <626085696@qq.com>
2025-05-11 09:51:02 +0000

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit e930d11a86a5b6e0193fd65bd5b4ea5dc9b55535.

--- src/main/java/com/example/secaicontainerengine/SecAiContainerEngineApplication.java
+++ src/main/java/com/example/secaicontainerengine/SecAiContainerEngineApplication.java
@@ -3,7 +3,9 @@ package com.example.secaicontainerengine;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @MapperScan("com.example.secaicontainerengine.mapper")
 public class SecAiContainerEngineApplication {
