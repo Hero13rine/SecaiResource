42069a032691b9739a53f9d594c59a81573ca999
HeFasheng <2973051203@qq.com>
2025-05-11 13:22:06 +0800

    2024-5-11:新增相关EvaluationConfig适配模型配置文件生成

---
+++ src/main/java/com/example/secaicontainerengine/pojo/dto/model/EvaluationConfig.java
@@ -0,0 +1,30 @@
+package com.example.secaicontainerengine.pojo.dto.model;
+
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class EvaluationConfig {
+
+    // 模型网络名称
+    private String modelNetName;
+
+    // 模型权重文件名
+    private String weightFileName;
+
+    // 模型网络文件名
+    private String modelNetFileName;
+
+    // 模型框架
+    private String framework;
+
+    // 模型任务类型
+    private String task;
+
+    // 模型评测方法
+    private List<String> evaluateMethods;
+
+
+}
